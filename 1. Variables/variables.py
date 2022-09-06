16114a07d46fa649ab4890e66d8f069b2f119184
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-06 16:14:29 -0400

    Module 1: Variables.

---
+++ 1. Variables/variables.py
@@ -0,0 +1,29 @@
+'''
+Variables are a reserved space in memory that contains a
+specified number of characters, that is saved for using later
+when the variable is called.
+'''
+
+var1 = "Hello, I'm learning Python"
+print(var1)
+
+#Python is dinamically typed, it means variables can be named
+#and replace their values anytime in the program
+#For example:
+print('----------')
+var2 = 1234
+print(var2)
+var2 = 5678
+print(var2)
+
+print('----------')
+var3 = 22.22
+print(var3)
+var3 = 1111.4444
+print(var3)
+
+print('----------')
+var4 = True
+print(var4)
+var4 = False
+print(var4)
