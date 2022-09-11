db5106be030a1e39a5ff20d112435277e29afa8f
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-11 19:59:08 -0400

    Module 3: Operators.

---
+++ 3. Operators/3.3 Relational Operators/4-greater_or_equal.py
@@ -0,0 +1,20 @@
+'''
+The >= operator is used for indicating that a data type has a
+greater or equal value than the one it's being compared to
+'''
+
+#it returns True if the first data type is equal than the second
+#one or if it is greater
+print(2 >= 2)
+print(3 >= 3)
+print(10 >= 6)
+print(50 >= 35)
+
+print('---------------------')
+
+#it returns False if the first data type is different than the
+#second one or if it is smaller 
+print(1 >= 2)
+print(2 >= 3)
+print(6 >= 10)
+print(30 >= 35)
