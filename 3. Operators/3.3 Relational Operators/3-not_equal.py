d28b60037363be602280bd4f834ff16014b36b9a
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-11 19:38:23 -0400

    != Operator.

---
+++ 3. Operators/3.3 Relational Operators/3-not_equal.py
@@ -0,0 +1,23 @@
+'''
+The operator != is used for comparing two data types that
+have a  different value
+'''
+
+#it returns True if both data types are different
+print(10 != 5)
+
+#it returns False if both data types are equal
+print(10 != 10)
+
+print('------------------')
+
+#this logic can be applied to variables
+a = 1
+b = 1
+c = 2
+d = 2
+
+print(a != b)
+print(a != c)
+print(b != d)
+print(c != d)
