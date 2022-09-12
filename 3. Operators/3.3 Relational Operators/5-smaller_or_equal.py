f076acf07f5bc4fbae9e44eb43c3b056b251c883
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-11 20:04:53 -0400

    Module 3: Operators.

---
+++ 3. Operators/3.3 Relational Operators/5-smaller_or_equal.py
@@ -0,0 +1,20 @@
+'''
+The <= operator is used for indicating that a data type has a
+smaller or equal value than the one it's being compared to
+'''
+
+#it returns True if the first data type is equal than the second
+#one or if it is smaller
+print(1 <= 2)
+print(2 <= 3)
+print(3 <= 6)
+print(30 <= 35)
+
+print('---------------------')
+
+#it returns False if the first data type is different than the
+#second one or if it is greater
+print(4 <= 2)
+print(6 <= 3)
+print(14 <= 10)
+print(50 <= 35)
