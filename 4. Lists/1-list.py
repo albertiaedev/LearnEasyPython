8046992e085adf3d1c431dee2f04e649591b5efb
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-13 18:44:33 -0400

    Module 4: Lists.

---
+++ 4. Lists/1-list.py
@@ -0,0 +1,43 @@
+'''
+Lists are used to store multiple items in a single variable and
+are created using square brackets.
+'''
+
+fruits = ['apple','banana','orange','grapes','lemon']
+print(fruits)
+print('----------------')
+cars = ['toyota','ford','chevrolet','hyundai','volkswagen']
+print(cars)
+
+print('----------------')
+print('----------------')
+
+#List items are:
+                #ordered
+                #changeable
+                #allow duplicate values
+
+#list are indexed as [0]...[n]
+
+'''
+Ordered means that the items have a defined order
+that cannot be chenged.
+Changeable means that we can change, add or remove items in
+the list after it's been created.
+Allow duplicates means that list can have items with the same
+value, since they're indexed.
+'''
+
+#lists support all kinds of data types
+
+int_list = [1,2,3,4,5,6]
+print(int_list)
+print('----------------')
+float_list = [1.1,2.2,3.3,4.4]
+print(float_list)
+print('----------------')
+str_list = ['apple','banana','orange','grapes','lemon']
+print(str_list)
+print('----------------')
+bool_list = [True, False, True, False]
+print(bool_list)
