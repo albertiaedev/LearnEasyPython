94889e0cc66d2f6abbcd96c5b749a66f79cb5654
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-14 18:28:31 -0400

    Module 5: Dictionaries.

---
+++ 5. Dictionaries/5-nested_dictionaries.py
@@ -0,0 +1,21 @@
+'''
+A nested dictionary is a dictionary that contains several
+dictionaries.
+'''
+
+MostHomeRuns = {
+  "first" : {
+    "player" : "Barry Bonds",
+    "homeruns" : 762
+  },
+  "second" : {
+    "player" : "Hank Aaron",
+    "homeruns" : 755
+  },
+  "third" : {
+    "player" : "Babe Ruth",
+    "homeruns" : 714
+  }
+}
+print(MostHomeRuns)
+
