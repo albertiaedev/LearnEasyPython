a210eed7ad98c548cd0469a7542268472683c111
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-15 19:28:04 -0400

    Module 7: Conditionals.

--- 7. Conditionals/7.1 If/1-if_statement.py
+++ 7. Conditionals/7.1 If/1-if_statement.py
@@ -1,8 +1,28 @@
 '''
-If you have only one statement to execute,
-you can put it on the same line as the if statement.
+An conditional statement is used to determine wheter a condition
+is met or not. This time, we'll talk about the if statement, which
+is written using the if keyword and accepts all types of operators:
+arithmetic, relational and logical.
 '''
 
-num1 =  20
-num2 = 10
-if num1 > num2: print("20 is greater than 10")
+a = 50
+b = 100
+if b > a:
+  print("b is greater than a") #move print() with indentation
+                               #this is done using 2-4 spaces
+
+print('-------------------')
+
+a = 50
+b = 100
+if b == a:
+  print("b is equal to a") #move print() with indentation
+                           #this is done using 2-4 spaces
+
+#you have to use indentation, instead of the typical curly brackets
+print('-------------------')
+
+a = 50
+b = 100
+if b == a:
+print("b is equal to a") #otherwise you'll get an error
