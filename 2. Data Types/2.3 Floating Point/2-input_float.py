d7f22eab788c1e77e6a112e784a4bb1b480f4021
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-17 10:05:02 -0400

    Module 2: Data Types.

---
+++ 2. Data Types/2.3 Floating Point/2-input_float.py
@@ -0,0 +1,10 @@
+'''
+We can ask the user insert a value of his choice. In this case,
+we're asking for a floating point value which can be specified
+just by naming a variable that contains the word float and in round
+brackets we write the word input plus the action to perform in
+another set of round brackets.
+'''
+
+#For example:
+input = float(input("Enter a floating point number: "))
