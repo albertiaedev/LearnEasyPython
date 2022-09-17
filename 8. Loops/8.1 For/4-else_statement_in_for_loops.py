80c4803b6987c239dd5089ca7516977e95a9f1e6
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-17 15:18:26 -0400

    Module 8: Loops.

---
+++ 8. Loops/8.1 For/4-else_statement_in_for_loops.py
@@ -0,0 +1,10 @@
+'''
+The else keyword in a for loop
+specifies a block of code to be
+executed when the loop is finished.
+'''
+
+for x in range(20):
+  print(x)
+else:
+  print("This loop has already done all iterations.")
