1a2718e1b692630d5d00bbe5e7d5ea3788aab3cd
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-17 10:03:56 -0400

    Module 2: Data Types.

---
+++ 2. Data Types/2.2 Integers/2-input_int.py
@@ -0,0 +1,10 @@
+'''
+We can ask the user insert a value of his choice. In this case,
+we're asking for an integer value which can be specified just by
+naming a variable that contains the word int and in round brackets
+we write the word input plus the action to perform in another set
+of round brackets.
+'''
+
+#For example:
+input = int(input("Enter an integer number: "))
