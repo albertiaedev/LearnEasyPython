fa1d868cd44a5965166ac7068489549d4f6d8458
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-17 10:02:23 -0400

    Module 2: Data Types.

---
+++ 2. Data Types/2.1 Strings/2-input_str.py
@@ -0,0 +1,9 @@
+'''
+We can ask the user insert a value of his choice. In this case,
+we're asking for a string value which can be specified just by
+naming a variable that contains the word input and the action to
+perform in round brackets.
+'''
+
+#For example:
+input = input("Enter a string of characters: ")
