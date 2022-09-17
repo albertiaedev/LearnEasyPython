aa11807b3119bdaabbfc3be4acd4982b9037848c
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-17 15:47:08 -0400

    Module 8: Loops.

---
+++ 8. Loops/8.2 While/3-else_statement_in_while_loops.py
@@ -0,0 +1,11 @@
+'''
+With the else statement we can run a block of code once
+the condition is no longer True.
+'''
+
+i = 0
+while i <= 10:
+  print(i)
+  i += 1
+else:
+  print("i is no longer 10 or less than 10.")
