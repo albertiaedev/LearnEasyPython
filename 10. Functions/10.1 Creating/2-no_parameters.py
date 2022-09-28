0d86b62b94e6d7d1c5ef438dd29dc9e311438a93
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-28 16:41:04 -0400

    Module 10: Functions.

---
+++ 10. Functions/10.1 Creating/2-no_parameters.py
@@ -0,0 +1,12 @@
+'''
+You can create a function that uses no parameters
+doing the same thing that we have done before.
+'''
+
+def function_with_no_parameters():
+    print("This is a function with no parameters")
+    q = 100 + 200
+    print(f"Also we have a sum: {q}")
+
+#we call it the same way
+function_with_no_parameters()
