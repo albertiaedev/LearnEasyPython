c5313f4ecbc02e8f463cfd107cfac383cc088fb1
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-28 16:36:09 -0400

    Module 10: Functions.

---
+++ 10. Functions/10.1 Creating/1-create_a_function.py
@@ -0,0 +1,11 @@
+'''
+A function is a block of code which only runs when it is
+called. You can pass data, known as parameters,
+into a function. A function can return data as a result.
+'''
+
+def my_function():
+  print("Hello from a function")
+
+#calling it to run
+my_function ()
