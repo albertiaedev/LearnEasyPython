f24f5f603b767d6974d449282641338d3463fe4d
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-29 19:20:17 -0400

    Module 11: OOP.

--- 11. Object Oriented Programming (OOP)/11.2 Inheritance/1-inheritance.py
+++ 11. Object Oriented Programming (OOP)/11.2 Inheritance/1-inheritance.py
@@ -5,3 +5,24 @@ A PARENT CLASS is the class being inherited from, also called base class.
 
 A CHILD CLASS is the class that inherits from another class, also called derived class
 '''
+
+#CREATE A PARENT CLASS
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+object = Person("John", "Doe")
+object.printname()
+
+print('----------------------')
+
+#CREATE A CHILD CLASS
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
