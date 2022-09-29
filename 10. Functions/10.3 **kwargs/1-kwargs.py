ff916a2bd9dd2e75ea175c2873a2f3b05e10ec10
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-28 20:42:41 -0400

    Module 10: Functions.

---
+++ 10. Functions/10.3 **kwargs/1-kwargs.py
@@ -0,0 +1,9 @@
+'''
+You can also send arguments with the key = value syntax.
+This way the order of the arguments does not matter.
+'''
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus") 
