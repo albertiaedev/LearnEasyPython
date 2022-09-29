9d9744b3182588e8f27e4e90369d7c9828a09060
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-29 19:07:56 -0400

    Module 11: OOP.

---
+++ 11. Object Oriented Programming (OOP)/11.2 Inheritance/1-inheritance.py
@@ -0,0 +1,7 @@
+'''
+Inheritance allows us to define a class that inherits all the methods and properties from another class.
+
+A PARENT CLASS is the class being inherited from, also called base class.
+
+A CHILD CLASS is the class that inherits from another class, also called derived class
+'''
