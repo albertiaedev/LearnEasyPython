fd4429afb3fcb8952b2c2a67e6771debb851949b
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-29 18:09:45 -0400

    Module 11: OOP.

---
+++ 11. Object Oriented Programming (OOP)/11.1 Classes-Objects/1-classes.py
@@ -0,0 +1,16 @@
+'''
+Almost everything in Python is an object,
+with its properties and methods.
+A Class is like an object constructor,
+or a "blueprint" for creating objects.
+'''
+
+#to create a class, use the keyword 'class'
+
+class Example: #use capital letters to name a class
+  var1 = 100
+
+#now we can use the class named MyClass to create objects
+
+object = Example()
+print(object.var1)
