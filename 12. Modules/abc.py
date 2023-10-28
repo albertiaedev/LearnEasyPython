c33340d6b75a40890d146b641eee9da96d627a3b
Alberto <albertiae.digital@gmail.com>
2023-10-28 12:52:27 -0400

    Module 12: Modules

---
+++ 12. Modules/abc.py
@@ -0,0 +1,22 @@
+# Abstract base classes (ABCs) provide a way to define interfaces that concrete classes must implement.
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+dog = Dog()
+cat = Cat()
+
+dog.speak()
+cat.speak()
