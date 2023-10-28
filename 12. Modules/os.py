c414b05d710e37d7c32438d535804922a4215917
Alberto <albertiae.digital@gmail.com>
2023-10-28 15:40:22 -0400

    Module 12: Modules

--- 12. Modules/os.py
+++ 12. Modules/os.py
@@ -1,13 +1,18 @@
 # Getting the current working directory
-
 import os
 
 cwd = os.getcwd()
 
 print(cwd)
 
-# Changing the current working directory
 
+# Changing the current working directory
 import os
 
 os.chdir("/path/to/new/directory")
+
+
+# Creating a directory
+import os
+
+os.mkdir("new_directory")
