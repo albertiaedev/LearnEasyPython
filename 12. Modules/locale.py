f2925ffb8e94890bbee579cb110f1eb025367c07
Alberto <albertiae.digital@gmail.com>
2023-11-02 17:34:18 -0400

    Module 12: Modules

---
+++ 12. Modules/locale.py
@@ -0,0 +1,9 @@
+# The _locale module provides classes and functions for working with locales.
+
+import _locale
+
+# Get the current locale
+locale = _locale.getdefaultlocale()
+
+# Print the locale
+print(locale)
