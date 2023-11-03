6067d8cd4fefb33c8f44909ee90cd8415c16a0ec
Alberto <albertiae.digital@gmail.com>
2023-11-03 11:04:33 -0400

    Module 12: Modules

---
+++ 12. Modules/operator.py
@@ -0,0 +1,7 @@
+# The _operator module provides functions for performing various operations on objects, such as addition, subtraction, and multiplication.
+
+import _operator
+
+print(_operator.add(4, 4))
+print(_operator.sub(5, 6))
+print(_operator.mul(10, 20))
