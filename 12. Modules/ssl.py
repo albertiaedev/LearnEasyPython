f39050ee7f8b53bcf5f31290bdb50207348805d8
Alberto <albertiae.digital@gmail.com>
2023-11-04 19:12:30 -0400

    Module 12: Modules

--- 12. Modules/ssl.py
+++ 12. Modules/ssl.py
@@ -1,3 +1,7 @@
 # The _ssl module provides classes and functions for implementing secure sockets.
 
 import _ssl
+
+
+# Create a secure socket
+socket = _ssl.SSLContext().wrap_socket(socket.socket())
