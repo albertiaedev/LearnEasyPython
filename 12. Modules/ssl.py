4125bf36c75e62b8d063143c0414951df3aff038
Alberto <albertiae.digital@gmail.com>
2023-11-04 19:14:00 -0400

    Module 12: Modules

--- 12. Modules/ssl.py
+++ 12. Modules/ssl.py
@@ -11,3 +11,6 @@ socket.connect(("example.com", 443))
 
 # Send data to the server
 socket.sendall("GET / HTTP/1.1\r\nHost: example.com\r\n\r\n".encode())
+
+# Receive data from the server
+response = socket.recv(1024)
